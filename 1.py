4cd7137aed85b5abb5b2233f732f49cdbcfd234b
Edgar-Mont-Cr <edgmoncr@gmail.com>
2025-02-26 11:53:28 -0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+print ("hola mundo");
+n1 = float(input ("ingresa el primer numero ----->"))
+n2 = float(input("ingresa el segundo numero ----->"))
+x=n1 + n2
+print("el resultado es ------>", x)
+
+nombres = (input("dime tu nombre ------->"))
+print("tu nombre es ------>" , nombres)
+#hola
+
+ap=(input("dime tu apellido paterno ------->"))
+am=(input("dime tu apellido materno ------->"))
+print (nombres +" "+ ap +" " + am)
+print("hola mundo")
