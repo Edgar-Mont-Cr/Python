d7d66ee0c0ddc779d3e21e1e2f803ffcb07449e5
Edgar-Mont-Cr <edgmoncr@gmail.com>
2025-02-26 11:53:10 -0600

    Add files via upload

---
+++ ejercicio 2.py
@@ -0,0 +1,10 @@
+x=int(input("dime que tabal deceas"))
+a=10
+while a >= 1:
+    print(f" {x} * {a} = {x*a}")
+    a-=1
+
+    #hola
+    ''' hola mundo
+    entero de mexico
+    y sus alrrededores'''
