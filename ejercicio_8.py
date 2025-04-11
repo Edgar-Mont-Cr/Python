53e95a1b7062729fed46015237431b7a9dd5a755
Edgar-Mont-Cr <edgmoncr@gmail.com>
2025-04-11 07:23:44 -0600

    Add files via upload

---
+++ ejercicio_8.py
@@ -0,0 +1,11 @@
+n1=int(input(print("Ingresa tu calificacion")))
+if n1>=100:
+    print("exelente felicitCIONES")
+elif n1<=99 and n1>=90:
+    print("MUY bien")
+elif n1<=89 and n1>=80:
+    print("BIEN")
+elif n1<=79 and n1>=70:
+    print("alumno regular")
+else:
+    print("alumno no aprobado")
